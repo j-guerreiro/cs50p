ea146b262e32a77ac628e7e0a38f88cf8c17a920
jguerreiro <joel.borghi@gmail.com>
2024-12-13 16:22:16 -0300

    Problem set extensions solution

--- problem-set-1/extensions.py
+++ problem-set-1/extensions.py
@@ -5,7 +5,25 @@ extensions_dict = {
 ".png": "image/png",
 ".pdf": "application/pdf",
 ".txt": "text/plain",
-".zip": "application/zip"
+".zip": "application/zip",
+"no-suffix": "application/octet-stream"
 }
 
-print(extensions_dict[".gif"])
+fileName = input("File name: ")
+
+if ".gif" in fileName:
+    print(extensions_dict[".gif"])
+elif ".jpg" in fileName:
+    print(extensions_dict[".jpg"])
+elif ".jpeg" in fileName:
+    print(extensions_dict[".jpeg"])
+elif ".png" in fileName:
+    print(extensions_dict[".png"])
+elif ".pdf" in fileName:
+    print(extensions_dict[".pdf"])
+elif ".txt" in fileName:
+    print(extensions_dict[".tx"])
+elif ".zip" in fileName:
+    print(extensions_dict[".zip"])
+else:
+    print(extensions_dict["no-suffix"])
