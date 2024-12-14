8a7cca14705226e567791770985b1435304dfd5e
jguerreiro <joel.borghi@gmail.com>
2024-12-14 19:19:11 -0300

    Extra problem set

---
+++ problem-set-1/number-check.py
@@ -0,0 +1,12 @@
+x = 1
+y = 1
+
+if x > y:
+    print(f"x({x}) is greater than y({y})")
+elif x < y:
+    print(f"x({x}) is smaller than y({y})")
+elif x != y:
+    print("They are not equal")
+else:
+    print("They are equal!")
+
