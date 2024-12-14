e032a0e9167a2e2ee38eb049659670e60e3e3fb2
jguerreiro <joel.borghi@gmail.com>
2024-12-14 15:47:39 -0300

    Bugfixes - still improving the solution

--- problem-set-1/interpreter.py
+++ problem-set-1/interpreter.py
@@ -15,40 +15,45 @@ if expression.find("/") != -1:
     print(result)
 
 elif expression.find("-", 1) != -1: # find the operator starting from index 1
-    minusDelimiter = expression.find("-")
+    minusDelimiter = expression.find("-",1)
 
     # left hand negative number check
-    if expression[0] == "-":
-      expression = expression[1:]
-      a = expression[minusDelimiter]
-      a = int(a)
-      a = a * -1
-    else:    
-        a = expression[:minusDelimiter]
-        a = int(a)
-        
-    b = expression[minusDelimiter + 1:]
-    b = int(b)
+    # if expression[0] == "-":
+    #   expression = expression[1:]
+    #   a = expression[minusDelimiter]
+    #   a = int(a)
+    #   a = a * -1
+    # else:    
+    #     a = expression[:minusDelimiter]
+    #     a = int(a)
 
-    result =  a - b
+    a = expression[:minusDelimiter]
+    b = expression[minusDelimiter + 1:]
+    a = int(a)
+    b = int(b)
+        
+    # b = expression[minusDelimiter + 1:]
+    # b = int(b)
+
+    result = round(a - b, 1)
     print(result)
 
-elif expression.find("*") != -1:
+elif expression.find("*", 1) != -1:
     multiplicationDelimiter = expression.find("*")
     a = expression[:multiplicationDelimiter]
     b = expression[multiplicationDelimiter + 1:]
     a = int(a)
     b = int(b)
-    result =  a * b
+    result = round(a * b, 1)
     print(result)
 
-elif expression.find("+") != -1:
-    plusDelimiter = expression.find("+")
+elif expression.find("+", 1) != -1:
+    plusDelimiter = expression.find("+",1)
     a = expression[:plusDelimiter]
     b = expression[plusDelimiter + 1:]
     a = int(a)
     b = int(b)
-    result =  a + b
+    result =  round(a + b, 1)
     print(result)
 
 #TODO fix negative number input ex: with sign in front: -1 + 100
