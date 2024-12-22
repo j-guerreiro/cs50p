28c27e851e390d12549d265cd55dc0a61de685f5
jguerreiro <joel.borghi@gmail.com>
2024-12-22 12:18:30 -0300

    Bug found - refactor applied to check_first_two function - try/catch block added to perform validation

--- problem-set-2/plates.py
+++ problem-set-2/plates.py
@@ -15,7 +15,8 @@ import string
     “Numbers cannot be used in the middle of a plate; they must come at the end. 
     For example, AAA222 would be an acceptable … vanity plate; AAA22A would not be acceptable. The first number used cannot be a '0'.”
     “No periods, spaces, or punctuation marks are allowed.”
-    In plates.py, implement a program that prompts the user for a vanity plate and then output Valid if meets all of the requirements or Invalid if it does not. 
+
+    Implement a program that prompts the user for a vanity plate and then output Valid if meets all of the requirements or Invalid if it does not. 
     Assume that any letters in the user's input will be uppercase. 
     Structure your program per the below, wherein is_valid returns True if s meets all requirements and False if it does not. Assume that s will be a str. 
     You're welcome to implement additional functions for is_valid to call (e.g., one function per requirement).
@@ -25,9 +26,9 @@ import string
 def main():
     plate = input("Plate: ")
     if is_valid(plate):
-        print("Valid")
+        print("Valid plate")
     else:
-        print("Invalid")
+        print("Invalid plate")
 
 
 def check_length(s):
@@ -36,13 +37,27 @@ def check_length(s):
 
 
 def check_first_two(s):
-    # check if the first two characters are not numbers
-    first_two_type = str(type(s[0:2]))
-    return  first_two_type == "<class 'str'>"
+    # check if the first two characters are not numbers.
+    numbers_found = 0
+
+    for char in range(0,1):
+        try:
+            int(char)
+        except:
+            print("Strings cannot be converted to int")
+        else:
+            print("A number was found, flagging as FALSE")
+            numbers_found = numbers_found + 1
+            
+    """  
+       if numbers are found, return false
+       if numbers are NOT found, return true
+    """
+    return not(numbers_found > 0)
 
 
 def check_illegal_chars(s):
-    # No periods, spaces, or punctuation marks are allowed.
+    # Check for: periods, spaces, or punctuation marks.
     illegal_chars = string.punctuation
     counter = 0
     
@@ -58,6 +73,44 @@ def check_illegal_chars(s):
             counter =  counter + 1
 
     return counter == 0
+
+def check_number_pattern(s):
+    """
+        # KEY RULES:
+
+        If the plate contains any numbers, they cannot appear in the middle of the plate or at the beginning.
+        Example:
+        Numbers must be at the end of the plate:
+
+        If the plate contains any numbers, they cannot appear in the middle of the plate or at the beginning.
+        Example:
+        ✅ ABC123 (Valid, numbers are at the end).
+        ❌ AB1C23 (Invalid, numbers are in the middle).
+        ❌ 1ABC23 (Invalid, numbers are at the beginning).
+        No numbers in the middle or mixed with letters:
+
+        Numbers cannot interrupt letters; they must all come after all the letters.
+        Example:
+        ✅ AAA222 (Valid, letters first, numbers at the end).
+        ❌ AAA22A (Invalid, numbers appear before another letter).
+        The first number cannot be a '0':
+
+        If numbers are included in the plate, they cannot start with a 0.
+        Example:
+        ✅ ABC123 (Valid, first number is not 0).
+        ❌ ABC012 (Invalid, the first number is 0).
+
+    """
+
+    # beginning_check = check_first_two(s)
+
+    # string_list = list(s)
+
+    # string_list = []
+
+    # After a letter, there can't be a number next
+    # AB1C23 -> B1C -> number is in the middle
+    # A1C, A1
 
 
 def is_valid(s):
